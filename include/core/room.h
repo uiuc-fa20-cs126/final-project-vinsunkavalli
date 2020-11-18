3b91866ddb7f3a5e0c94725ff3d321a2f6a63baf
vinsunkavalli <vinsunkavalli@gmail.com>
2020-11-17 20:30:55 -0800

    Finished writing code for room

--- include/core/room.h
+++ include/core/room.h
@@ -2,6 +2,9 @@
 #define FINAL_PROJECT_VINSUNKAVALLI_ENGINE_INCLUDE_CORE_ROOM_H_
 
 #include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
 #include <glm/glm.hpp>
 
 class Room{
@@ -13,11 +16,11 @@ class Room{
   Room();
   Room(std::vector<std::vector<int>> map);
 
-  friend std::istream& operator>>(std::istream& in, const Room& room);
-
   std::vector<std::vector<int>> getRoomMap();
 
   void setRoomMap(std::vector<std::vector<int>> map);
+
+  friend std::istream& operator>>(std::istream& in, Room& room);
 };
 
 #endif //FINAL_PROJECT_VINSUNKAVALLI_ENGINE_INCLUDE_CORE_ROOM_H_
--- src/core/room.cpp
+++ src/core/room.cpp
@@ -12,3 +12,29 @@ void Room::setRoomMap(std::vector<std::vector<int>> map) {
   room_map_ = map;
 }
 
+std::istream& operator>>(std::istream& in, Room& room) {
+  std::string row_string;
+  std::vector<std::vector<int> > new_map;
+  float boundX, boundY;
+
+  //first two lines are always width then height
+  std::getline(in, row_string);
+  boundX = std::stof(row_string);
+  std::getline(in, row_string);
+  boundY = std::stof(row_string);
+
+  for(auto i = 0; i < boundY; ++i) {
+    std::vector<int> map_row;
+    std::getline(in, row_string);
+    for(auto j = 0; j < boundX; ++j) {
+      std::string num = reinterpret_cast<const char *>(row_string.at(j));
+      map_row.push_back(std::stoi(num));
+    }
+
+    new_map.push_back(map_row);
+  }
+
+  room.setRoomMap(new_map);
+
+  return in;
+}
